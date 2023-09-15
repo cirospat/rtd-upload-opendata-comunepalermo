13286cfd084f37c9ff9a61b566e80c9ddce97fa2
ciro spataro <cirospat@gmail.com>
2023-09-15 11:39:17 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ extlinks = {}
 
 # -- Options for HTML output ---------------------------------------------------
 
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['static']
 
